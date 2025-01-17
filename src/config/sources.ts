283565ad577275e052c103f4f16f5667467e3ef5
Julian Gernun <17549662+jcger@users.noreply.github.com>
2025-01-17 16:33:43 +0100

    fake api

--- src/config/sources.ts
+++ src/config/sources.ts
@@ -9,16 +9,10 @@ export interface ChangelogSource {
 
 export const changelogSources: ChangelogSource[] = [
   {
-    name: "Jira Cloud",
-    key: "jira_cloud",
-    url: "https://developer.atlassian.com/cloud/jira/platform/changelog/",
+    name: "Fake API",
+    key: "fake_api",
+    url: "https://fake-api.com/changelog",
     cardSelector:
       "#changelogContentWrapperTop > div:nth-child(2) > div:nth-child(1) > div",
-  },
-  {
-    name: "Slack",
-    key: "slack",
-    url: "https://api.slack.com/changelog",
-    cardSelector: ".apiChangelog__group",
   },
 ];
